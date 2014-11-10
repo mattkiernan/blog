2f0661aeecc072dc926ae5312840b41dca0c6ad8
mattkiernan <matt.kiernan@gmail.com>
2014-11-10 05:36:58 -0500

    Allow user to delete posts

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -33,6 +33,12 @@ class PostsController < ApplicationController
     end
   end
 
+  def destroy
+    post = Post.find(params[:id])
+    post.destroy
+    redirect_to root_path
+  end
+
   def post_params
     params.require(:post).permit(:title, :body, :description)
   end
