d74bac2cd7e0756a5e907ba02d7d4e935d9d38e7
mattkiernan <matt.kiernan@gmail.com>
2014-11-13 13:01:23 -0500

    Migrate to postgresql

--- db/schema.rb
+++ db/schema.rb
@@ -13,6 +13,9 @@
 
 ActiveRecord::Schema.define(version: 20141110110212) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "pages", force: true do |t|
     t.string   "name"
     t.string   "permalink"
@@ -36,6 +39,6 @@ ActiveRecord::Schema.define(version: 20141110110212) do
     t.datetime "updated_at",      null: false
   end
 
-  add_index "users", ["email"], name: "index_users_on_email", unique: true
+  add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
 
 end
