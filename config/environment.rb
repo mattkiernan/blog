4834a841b899b8befd1b260e117ebd9a0446dc5d
mattkiernan <matt.kiernan@gmail.com>
2015-02-24 11:33:53 -0500

    Add Logentries env

--- config/environment.rb
+++ config/environment.rb
@@ -3,3 +3,11 @@ require File.expand_path('../application', __FILE__)
 
 # Initialize the Rails application.
 Rails.application.initialize!
+
+if Rails.env.development?
+    Rails.logger = Le.new('89239e43-e535-4b91-be24-07dbfd84b7f1', debug: true)
+else
+    Rails.logger = Le.new('89239e43-e535-4b91-be24-07dbfd84b7f1')
+end
+
+Rails.logger.info
